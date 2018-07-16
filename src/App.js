ed1651107d549efe23e6ce5dcc268b032e14ef69
Pablo Lam <pablolam709@gmail.com>
2018-07-16 13:01:43 -0400

    BT/PL - newsListTable component (WIP)

--- src/App.js
+++ src/App.js
@@ -13,9 +13,42 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <NewsListTable/>
+
       </div>
     );
   }
+}
+
+class NewsListTable extends Component {
+  contructor(props) {
+    super(props);
+    this.state = {
+      title: "asdfasdf",
+      date: "July 19 2018",
+      user: "baran"
+  };
+}
+  render() {
+    return (
+      <div>
+        <h1>newsList</h1>
+        <NewsRows title={this.state.title} date={this.state.date} user={this.state.user}/>
+      </div>
+    );
+  }
+}
+
+function NewsRow(props) {
+  return (
+    <div>
+      newsrow:
+      {this.props.title}
+      {this.props.date}
+      {this.props.user}
+    </div>
+  );
 }
 
 export default App;
