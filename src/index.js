befde46766d0335f9b3222d9572b137f3adfacb6
Peter Altosaar <peter.altosaar@gmail.com>
2018-07-18 18:46:09 -0400

    Add first hacky attempt at a search component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Search from './Search'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Search />, document.getElementById('root'));
 registerServiceWorker();
