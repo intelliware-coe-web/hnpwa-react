feeb2a571b9d4e3d5c70d50569e30367bda4c2ad
Jean Bourgeois <bourgeoisj@mac-1987.intware.com>
2018-07-31 15:47:25 -0400

    Fixed tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,25 +3,15 @@ import ReactDOM from 'react-dom';
 import store from '../../store/index';
 import { Provider } from 'react-redux';
 import App from './App';
-import { Provider } from 'react-redux';
-import UserProfileReducer from './reducers/UserProfileReducer'
-import { createStore } from 'redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-<<<<<<< HEAD:src/App.test.js
-  const store = createStore(UserProfileReducer);
 
   ReactDOM.render(
     <Provider store = {store}>
       <App />
     </Provider>
   , div);
-=======
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>, div);
->>>>>>> 1d18852e6791d54b036cb2b90f6784e4f4a2e2e1:src/components/App/App.test.js
+
   ReactDOM.unmountComponentAtNode(div);
 });
