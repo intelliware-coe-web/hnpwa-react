ea738ba5a95820f6c27182a75991171be1d9d279
Jean-Louis Bourgeois <lou@Jean-Louiss-iMac.local>
2018-08-11 14:53:37 -0400

    Added Redux dev tools

--- src/store.js
+++ src/store.js
@@ -6,10 +6,14 @@ import newsReducer from './actions/news.reducer';
 const rootReducer = combineReducers({
     search: searchReducer,
     user: userReducer,
-    news: newsReducer,
+    news: newsReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 window.store = store;
 
 export default store;
