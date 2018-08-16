4397911b1b104b22aaff729ef71e928e378cad50
Leandro Siqueira <38536+lsiqueira@users.noreply.github.com>
2018-08-16 10:47:15 -0400

    add userReducer unit tests

---
+++ src/__tests__/actions/user.reducer.test.js
@@ -0,0 +1,22 @@
+import userReducer from '../../actions/user.reducer';
+import { initialState } from '../../actions/user.reducer';
+
+
+describe('userReducer', () => {
+
+    it('should return the initial state', () => {
+        expect(userReducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should handle USER_MAKE_CAT and update avatar property', () => {
+        let state = userReducer(undefined, { type: 'USER_MAKE_CAT' });
+
+        expect(state.profile.avatar).toBeGreaterThanOrEqual(1);
+        expect(state.profile.avatar).toBeLessThanOrEqual(6);
+        
+        expect(state.profile.username).toEqual(initialState.profile.username);
+        expect(state.profile.description).toEqual(initialState.profile.description);
+        expect(state.profile.createdDate).toEqual(initialState.profile.createdDate);
+        expect(state.profile.numOfStories).toEqual(initialState.profile.numOfStories);    
+    });
+});
--- src/actions/user.reducer.js
+++ src/actions/user.reducer.js
@@ -1,4 +1,4 @@
-const initialState = {
+export const initialState = {
     //Mock initial state
     profile: {
         avatar:undefined,
